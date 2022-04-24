0dbc37be60df595065d75a85280f74380a82fabe
youlifil <youlifil@gmail.com>
2022-04-25 00:47:40 +0300

    initial commit: .gitignore, setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""setup.py for yf_kazan_test package
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='yf_kazan_test',
+    version='0.0.1',
+    description="Yury Filimonov test project for Kazan Express",
+    long_description=(''),
+    author='Yury Filimonov',
+    author_email='yury.filimonov@hotmail.com',
+    url='https://github.com/youlifil/yf_kazan_test',
+    keywords=[],
+    entry_points={},
+    install_requires=[
+    ],
+    packages=find_packages(),
+    license='')
