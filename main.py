31c1f04b490923dd6e5af267995e3c8e5ad61405
youlifil <youlifil@gmail.com>
2022-04-25 13:42:18 +0300

    add category_tree and score packages

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from yf_kazan_test.data import load_datasets
+from yf_kazan_test.data import download_datasets, preprocessed_train_dataset, test_dataset
 from yf_kazan_test.util import print_md, catch_time
+from yf_kazan_test.category_tree import init_category_tree
 
 # load_datasets()
 
-import time
+train_df = preprocessed_train_dataset()
+test_df = test_dataset()
 
-with catch_time() as t:
-    time.sleep(3)
-print(t())
-
+init_category_tree()
--- yf_kazan_test/data.py
+++ yf_kazan_test/data.py
@@ -1,6 +1,8 @@
 import gdown
 import pandas as pd
 
+CLASS_NUM = None
+
 def download_datasets():
     gdown.download(id="1IsTN19WU00pcLAGf52Vp9_-xC-6c-49J")
     gdown.download(id="11r85SCXCJrfZ_vShAcyr8AJmjZd7lQsz")
@@ -10,6 +12,7 @@ def preprocessed_train_dataset():
     dataset_filename = "preprocessed_train.parquet"
     gdown.download(id="1P9exIDwqTYNa-kvLgFhRA7_1BhXAINWv", output=dataset_filename)
     train_df = pd.read_parquet(dataset_filename)
+    CLASS_NUM = len(set(train_df["category_id"]))
     return train_df
 
 def test_dataset():
