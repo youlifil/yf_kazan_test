6e1a73c6d929dbc256a741dec6c8f55eb3adabde
youlifil <youlifil@gmail.com>
2022-04-25 13:06:29 +0300

    [data] add preprocessed_train_dataset(), test_dataset()

--- yf_kazan_test/data.py
+++ yf_kazan_test/data.py
@@ -1,6 +1,16 @@
 import gdown
+import pandas as pd
 
-def load_datasets():
+def download_datasets():
     gdown.download(id="1IsTN19WU00pcLAGf52Vp9_-xC-6c-49J")
     gdown.download(id="11r85SCXCJrfZ_vShAcyr8AJmjZd7lQsz")
     gdown.download(id="1u2TqxNY9U9KGchC77_s2PGbOPHrCvoFf")
+
+def preprocessed_train_dataset():
+    dataset_filename = "preprocessed_train.parquet"
+    gdown.download(id="1P9exIDwqTYNa-kvLgFhRA7_1BhXAINWv", output=dataset_filename)
+    train_df = pd.read_parquet(dataset_filename)
+    return train_df
+
+def test_dataset():
+    return pd.read_parquet("test.parquet")
